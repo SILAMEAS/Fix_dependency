8b26c83e041f36a1b41940d7fc2a99c5e4d7578a
SILAMEAS <silam8422@gmail.com>
2022-12-28 10:58:05 +0700

    28(1,19,9)

---
+++ src/components/RootComponent.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { RootStack } from '../navigators/rootNavigator';
+
+export const Root = () => {
+	// const [lightBg, darkBg] = useToken(
+	// 	'colors',
+	// 	['coolGray.50', 'blueGray.900'],
+	// 	'blueGray.900',
+	// );
+	// const bgColor = useColorModeValue(lightBg, darkBg);
+
+	return (
+		<NavigationContainer>
+			{/*<Box*/}
+			{/*	flex={1}*/}
+			{/*	w="100px"*/}
+			{/*	_light={{*/}
+			{/*		bg: 'cyan.700',*/}
+			{/*	}}*/}
+			{/*	_dark={{*/}
+			{/*		bg: 'cyan.700',*/}
+			{/*	}}*/}
+			{/*	// bg={useColorModeValue('', 'blueGray.900')}*/}
+			{/*	_web={{*/}
+			{/*		overflowX: 'hidden',*/}
+			{/*	}}*/}
+			{/*>*/}
+			{/*	<RootStack />*/}
+			{/*</Box>*/}
+				<RootStack />
+		</NavigationContainer>
+	);
+};
---
+++ src/screens/index.tsx
@@ -0,0 +1,9 @@
+import { LoginScreen } from "./Login";
+import { ForgotPasswordScreen } from "./ForgotPassword";
+import { HomeScreen } from "./Home";
+
+export {
+  LoginScreen,
+  ForgotPasswordScreen,
+  HomeScreen
+}
